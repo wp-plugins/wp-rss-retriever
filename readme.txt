0bcb9a69c4d720a9bfe0786b426a4817d26bd488
TJTaylor <TJTaylor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-13 18:24:05 +0000

    version 1.1 - thumbnail and cache support
    git-svn-id: https://plugins.svn.wordpress.org/wp-rss-retriever/trunk@1067279 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: tjtaylor
 Donate link: http://travistaylor.com/wp-rss-retriever-demo/
 Tags: rss, rss retriever, wp rss retriever, rss aggregator, rss feed, rss fetch feed, rss fetch, fetch feed
 Requires at least: 2.8
-Tested up to: 3.8.1
-Stable tag: 1.0.2
+Tested up to: 4.1
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -24,13 +24,16 @@ This plugin fetchs an RSS feed and displays it in an unordered list using shortc
 	<li>Control whether links open in a new window or not</li>
 	<li>Simple, lightweight, and fast</li>
 	<li>Easy to setup</li>
+	<li><strong>*NEW*</strong> Fetch thumbnail or first image</li>
+	<li><strong>*NEW*</strong> Control size of thumbnail</li>
+	<li><strong>*NEW*</strong> Set cache time (in seconds)</li>
 </ul>
 
 <h4>Live Demo:</h4>
 <p><a href="http://travistaylor.com/wp-rss-retriever-demo/" target="_blank">http://travistaylor.com/wp-rss-retriever-demo/</a></p>
 
 <h4>Example:</h4>
-<pre><code>[wp_rss_retriever url="http://travistaylor.com/feed/" items="10"]</code></pre>
+<pre><code>[wp_rss_retriever url="http://feeds.feedburner.com/TechCrunch/" items="10" excerpt="50" read_more="true" new_window="true" thumbnail="200" cache="7200"]</code></pre>
 
 <h4>Properties:</h4>
 <ul>
@@ -38,17 +41,18 @@ This plugin fetchs an RSS feed and displays it in an unordered list using shortc
 
 	<li><strong>items</strong> - Number of items from the feed you wish to fetch <em>(Default is 10)</em></li>
 
-	<li><strong>excerpt</strong> - How many words you want to display for each item</br>
-	<em>(Default is 0 or infinite)</em></li>
+	<li><strong>excerpt</strong> - How many words you want to display for each item	<em>(Default is 0 or infinite)</em></li>
 
-	<li><strong>read_more</strong> - Whether to display a read more link or not</br>
-	<em>(true or false, defaults to true)</em></li>
+	<li><strong>read_more</strong> - Whether to display a read more link or not	<em>(true or false, defaults to true)</em></li>
 
-	<li><strong>new_window</strong> - Whether to open the title link and read more link in a new window</br>
-	<em>(true or false, defaults to true)</em></li>
+	<li><strong>new_window</strong> - Whether to open the title link and read more link in a new window	<em>(true or false, defaults to true)</em></li>
+
+	<li><strong>thumbnail</strong> - Whether or not you want to display a thumbnail, and if so, what size you want it to be<em>(true or false, defaults to true. Inserting a number will change the size, default is 150)</em></li>
+
+	<li><strong>cache</strong> - How long you want the feed to cache the results in seconds <em>(Default is 43200)</em></li>
 </ul>
 
-Note: This plugin does not include CSS. It was created so you could easily style the list to match your website. If you would like to see more features for the plugin, let me know.
+Note: If you would like to see more features for the plugin, let me know.
 
 == Installation ==
 
@@ -69,4 +73,11 @@ This section describes how to install the plugin and get it working.
 * Fixes bug where excerpt included html and broken images
 
 = 1.0.2 =
-* Pulls images in when excerpt is not enabled
+* Pulls images & html in when excerpt is not enabled
+
+= 1.1 =
+* Fixed several bugs
+* Fetch thumbnail or first image
+* Control size of thumbnail
+* Set cache time (in seconds)
+* Now includes small CSS file, required for thumbnail support
--- wp-rss-retriever.php
+++ wp-rss-retriever.php
@@ -3,11 +3,20 @@
  * Plugin Name: RSS Retriever
  * Plugin URI: http://wordpress.org/plugins/wp-rss-retriever/
  * Description: A lightweight RSS fetch plugin which uses the shortcode [wp_rss_retriever] to fetch and display an RSS feed in an unordered list.
- * Version: 1.0.2
+ * Version: 1.1
  * Author: Travis Taylor
  * Author URI: http://travistaylor.com/
  * License: GPL2
  */
+
+add_action( 'wp_enqueue_scripts', 'wp_rss_retriever_css');
+
+function wp_rss_retriever_css() {
+    global $post;
+    if( has_shortcode( $post->post_content, 'wp_rss_retriever') ) {
+        wp_enqueue_style('rss-retriever', plugin_dir_url( __FILE__) . 'inc/css/rss-retriever.css');
+    }
+}
 
 add_shortcode( 'wp_rss_retriever', 'wp_rss_retriever_func' );
 
@@ -17,10 +26,18 @@ function wp_rss_retriever_func( $atts, $content = null ){
 		'items' => '10',
 		'excerpt' => '0',
 		'read_more' => 'true',
-		'new_window' => 'true'
+		'new_window' => 'true',
+        'thumbnail' => 'false',
+        'cache' => '43200'
 	), $atts ) );
 
+    update_option( 'wp_rss_cache', $cache );
+
+    add_filter( 'wp_feed_cache_transient_lifetime', 'wp_rss_retriever_cache' );
+
     $rss = fetch_feed( $url );
+
+    remove_filter( 'wp_feed_cache_transient_lifetime', 'wp_rss_retriever_cache' );
 
     if ( ! is_wp_error( $rss ) ) :
 
@@ -34,33 +51,87 @@ function wp_rss_retriever_func( $atts, $content = null ){
 
     endif;
     $output = '<div class="wp_rss_retriever">';
-        $output .= '<ul>';
+        $output .= '<ul class="wp_rss_retriever_list">';
             if ( !isset($maxitems) ) : 
                 $output .= '<li>' . _e( 'No items', 'wp-rss-retriever' ) . '</li>';
             else : 
-                // Loop through each feed item and display each item.
+                //loop through each feed item and display each item.
                 foreach ( $rss_items as $item ) :
-                    $output .= '<li>';
-                        $output .= '<a ' . $newWindowOutput . 'href="' . esc_url( $item->get_permalink() ) . '"
-                            title="' . sprintf( __( 'Posted %s', 'wp-rss-retriever' ), $item->get_date('j F Y | g:i a') ) . '">';
-                            $output .= esc_html( $item->get_title() );
-                        $output .= '</a>';
-                        $content = $item->get_content();
-                        if ( $excerpt > 0 ) {
-                            $output .= '<p>' . esc_html(implode(' ', array_slice(explode(' ', strip_tags($content)), 0, $excerpt))) . "...";
-                        } else {
-                            $output .= '<p>' . $content;
+                    //variables
+                    $content = $item->get_content();
+                    $title = esc_html( $item->get_title() );
+                    $enclosure = $item->get_enclosure();
+
+                    //build output
+                    $output .= '<li class="wp_rss_retriever_item">';
+                        $output .= '<a class="wp_rss_retriever_title" ' . $newWindowOutput . 'href="' . esc_url( $item->get_permalink() ) . '"
+                            title="' . $title . '">';
+                            $output .= $title;
+                        $output .= '</a>';               
+                        if ($thumbnail != 'false' && $enclosure) {
+                            $thumbnail_image = $enclosure->get_thumbnail();                     
+                            if ($thumbnail_image) {
+                                //use thumbnail image if it exists
+                                $resize = wp_rss_retriever_resize_thumbnail($thumbnail);
+                                $class = wp_rss_retriever_get_image_class($thumbnail_image);
+                                $output .= '<div class="wp_rss_retriever_image"' . $resize . '><img' . $class . ' src="' . $thumbnail_image . '" alt="' . $title . '"></div>';
+                            } else {
+                                //if not than find and use first image in content
+                                preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $content, $first_image);
+                                if ($first_image){    
+                                    $resize = wp_rss_retriever_resize_thumbnail($thumbnail);                                
+                                    $class = wp_rss_retriever_get_image_class($first_image["src"]);
+                                    $output .= '<div class="wp_rss_retriever_image"' . $resize . '><img' . $class . ' src="' . $first_image["src"] . '" alt="' . $title . '"></div>';
+                                }
+                            }
                         }
-                        if( $read_more == 'true' ) {
-                            $output .= ' <a class="wp_rss_retriever_readmore" ' . $newWindowOutput . 'href="' . esc_url( $item->get_permalink() ) . '"
-                                    title="' . sprintf( __( 'Posted %s', 'wp-rss-retriever' ), $item->get_date('j F Y | g:i a') ) . '">';
-                                    $output .= __( 'Read more &raquo;', 'wp-rss-retriever' );
-                            $output .= '</a></p>';
+                        $output .= '<div class="wp_rss_retriever_container">';
+                        if ( $excerpt != 'none' ) {
+                            if ( $excerpt > 0 ) {
+                                $output .= esc_html(implode(' ', array_slice(explode(' ', strip_tags($content)), 0, $excerpt))) . "...";
+                            } else {
+                                $output .= $content;
+                            }
+                            if( $read_more == 'true' ) {
+                                $output .= ' <a class="wp_rss_retriever_readmore" ' . $newWindowOutput . 'href="' . esc_url( $item->get_permalink() ) . '"
+                                        title="' . sprintf( __( 'Posted %s', 'wp-rss-retriever' ), $item->get_date('j F Y | g:i a') ) . '">';
+                                        $output .= __( 'Read more &raquo;', 'wp-rss-retriever' );
+                                $output .= '</a>';
+                            }
                         }
-                    $output .= '</li>';
+                        $output .= '<div class="wp_rss_retriever_date">' . sprintf( __( 'Published: %s', 'wp-rss-retriever' ), $item->get_date('F j, Y - g:i a') ) . '</div>';
+                    $output .= '</div></li>';
                 endforeach;
             endif;
         $output .= '</ul>';
     $output .= '</div>';
+
     return $output;
+}
+
+add_option( 'wp_rss_cache', 43200 );
+
+function wp_rss_retriever_cache() {
+    //change the default feed cache
+    $cache = get_option( 'wp_rss_cache', 43200 );
+    return $cache;
+}
+
+function wp_rss_retriever_get_image_class($image_src) {
+    list($width, $height) = getimagesize($image_src);
+    if ($height > $width) {
+        $class = ' class="portrait"';
+    } else {
+        $class = '';
+    }
+    return $class;
+}
+
+function wp_rss_retriever_resize_thumbnail($thumbnail) {
+    if (is_numeric($thumbnail)){
+        $resize = ' style="width:' . $thumbnail . 'px; height:' . $thumbnail . 'px;"';
+    } else {
+        $resize = '';
+    }
+    return $resize;
 }
